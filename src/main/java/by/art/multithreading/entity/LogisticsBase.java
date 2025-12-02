9bde4f6c57532c5c0cb07f45eb282f86e9e29365
Aliaksandr Artsikhovich <art-orient@tut.by>
2025-12-02 09:08:48 +0300

    add part of base

--- src/main/java/by/art/multithreading/entity/LogisticsBase.java
+++ src/main/java/by/art/multithreading/entity/LogisticsBase.java
@@ -1,4 +1,25 @@
 package by.art.multithreading.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class LogisticsBase {
+  private final Terminal[] terminals;
+  private final int baseMaxCapacity;
+  private final double maxLoadFactor = 0.8;
+  private final double minLoadFactor = 0.2;
+  private AtomicInteger currentWeight;
+  private final List<Truck> trucks = new ArrayList<>();
+
+  public LogisticsBase(int terminalCount, int maxWeightCapacity) {
+    this.terminals = new Terminal[terminalCount];
+    for (int i = 0; i < terminalCount; i++) {
+      terminals[i] = new Terminal(i + 1);
+    }
+    this.baseMaxCapacity = maxWeightCapacity;
+    currentWeight = new AtomicInteger(maxWeightCapacity / 2);
+  }
+
+  //TODO
 }
